88b7251806f726df7a824a608bba13dcda4db398
Simon Rozsival <simon@rozsival.com>
2021-04-01 10:40:55 +0200

    :construction: Use native HTTP client

--- fluent-plugin-logtail.gemspec
+++ fluent-plugin-logtail.gemspec
@@ -20,7 +20,6 @@ Gem::Specification.new do |s|
   s.required_ruby_version = Gem::Requirement.new(">= 2.4.0".freeze)
 
   s.add_runtime_dependency('fluentd', '> 1', '< 2')
-  s.add_runtime_dependency('http', '~> 2.0', '>= 2.0.3')
 
   s.add_development_dependency('rspec', '~> 3.4')
   s.add_development_dependency('test-unit', '~> 3.3.9')
--- lib/fluent/plugin/out_logtail.rb
+++ lib/fluent/plugin/out_logtail.rb
@@ -1,4 +1,5 @@
 require 'fluent/output'
+require 'net/https'
 
 module Fluent
   class LogtailOutput < Fluent::BufferedOutput
@@ -6,34 +7,18 @@ module Fluent
 
     VERSION = "0.1.0".freeze
     CONTENT_TYPE = "application/msgpack".freeze
-    HOST = "https://in.logtail.com".freeze
+    HOST = "in.logtail.com".freeze
+    PORT = 443
     PATH = "/".freeze
     MAX_ATTEMPTS = 3.freeze
     RETRYABLE_CODES = [429, 500, 502, 503, 504].freeze
-    USER_AGENT = "Logtail Logstash/#{VERSION}".freeze
+    USER_AGENT = "Logtail Fluentd/#{VERSION}".freeze
 
     config_param :source_token, :string, secret: true
     config_param :ip, :string, default: nil
 
     def configure(conf)
-      source_token = conf["source_token"]
-      @headers = {
-        "Authorization" => "Bearer #{source_token}",
-        "Content-Type" => CONTENT_TYPE,
-        "User-Agent" => USER_AGENT
-      }
-      super
-    end
-
-    def start
-      super
-      require 'http'
-      HTTP.default_options = {:keep_alive_timeout => 29}
-      @http_client = HTTP.persistent(HOST)
-    end
-
-    def shutdown
-      @http_client.close if @http_client
+      @source_token = conf["source_token"]
       super
     end
 
@@ -52,11 +37,19 @@ module Fluent
           return false
         end
 
+        http = build_http_client
         body = chunk.read
-        response = @http_client.headers(@headers).post(PATH, body: body)
-        response.flush
-        code = response.code
 
+        begin
+          resp = http.start do |conn|
+            req = build_request(body)
+            conn.request(req)
+          end
+        ensure
+          http.finish if http.started?
+        end
+
+        code = resp.code.to_i
         if code >= 200 && code <= 299
           true
         elsif RETRYABLE_CODES.include?(code)
@@ -75,6 +68,27 @@ module Fluent
         sleep_for = attempt ** 2
         sleep_for = sleep_for <= 60 ? sleep_for : 60
         (sleep_for / 2) + (rand(0..sleep_for) / 2)
+      end
+
+      def build_http_client
+        http = Net::HTTP.new(HOST, PORT)
+        http.use_ssl = true
+        # Verification on Windows fails despite having a valid certificate.
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+        http.read_timeout = 30
+        http.ssl_timeout = 10
+        http.open_timeout = 10
+        http
+      end
+
+      def build_request(body)
+        path = '/'
+        req = Net::HTTP::Post.new(path)
+        req["Authorization"] = "Bearer #{@source_token}"
+        req["Content-Type"] = CONTENT_TYPE
+        req["User-Agent"] = USER_AGENT
+        req.body = body
+        req
       end
   end
 end
