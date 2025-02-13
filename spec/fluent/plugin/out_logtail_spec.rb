e4c8a9a68cc1dcc4f242df2988ce2e3921e6fc3d
Andrei Lazarescu <lazarescu.andrei19@gmail.com>
2025-02-13 10:30:03 +0200

    updated tests :white_check_mark:

--- spec/fluent/plugin/out_logtail_spec.rb
+++ spec/fluent/plugin/out_logtail_spec.rb
@@ -62,21 +62,6 @@ describe Fluent::LogtailOutput do
       expect(stub).to have_been_requested.times(1)
     end
 
-    describe "#write to cloud" do
-    it "should send a chunked request to the Logtail API" do
-      stub = stub_request(:post, "https://in.logs.betterstack.com/").
-        with(
-          :body => start_with("\xDD\x00\x00\x00\x01\x85\xA3age\x1A\xAArequest_id\xA242\xA9parent_id\xA6parent\xAArouting_id\xA7routing\xA2dt\xB4".force_encoding("ASCII-8BIT")),
-          :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer abcd1234', 'Content-Type'=>'application/msgpack', 'User-Agent'=>'Logtail Fluentd/0.1.1'}
-        ).
-        to_return(:status => 202, :body => "", :headers => {})
-
-      driver.emit(record)
-      driver.run
-
-      expect(stub).to have_been_requested.times(1)
-    end
-
     it "handles 500s" do
       stub = stub_request(:post, "https://in.logs.betterstack.com/").to_return(:status => 500, :body => "", :headers => {})
 
@@ -88,6 +73,22 @@ describe Fluent::LogtailOutput do
 
     it "handle auth failures" do
       stub = stub_request(:post, "https://in.logs.betterstack.com/").to_return(:status => 403, :body => "", :headers => {})
+
+      driver.emit(record)
+      driver.run
+
+      expect(stub).to have_been_requested.times(1)
+    end
+  end
+
+  describe "#write to cloud" do
+    it "should send a chunked request to the Logtail API" do
+      stub = stub_request(:post, "https://in.logs.betterstack.com/").
+        with(
+          :body => start_with("\xDD\x00\x00\x00\x01\x85\xA3age\x1A\xAArequest_id\xA242\xA9parent_id\xA6parent\xAArouting_id\xA7routing\xA2dt\xB4".force_encoding("ASCII-8BIT")),
+          :headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Authorization'=>'Bearer abcd1234', 'Content-Type'=>'application/msgpack', 'User-Agent'=>'Logtail Fluentd/0.1.1'}
+        ).
+        to_return(:status => 202, :body => "", :headers => {})
 
       driver.emit(record)
       driver.run
